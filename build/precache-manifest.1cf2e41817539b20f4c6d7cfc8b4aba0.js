e040f25ab974488864c9b356bb264a6b09bf98af
Michael Chen <mchen@glenair.com>
2022-08-04 12:04:34 -0700

    actual frontend

---
+++ build/precache-manifest.1cf2e41817539b20f4c6d7cfc8b4aba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b32a5ad735d4e11201",
+    "url": "/static/css/main.8a8fe52d.chunk.css"
+  },
+  {
+    "revision": "e4b32a5ad735d4e11201",
+    "url": "/static/js/main.e4b32a5a.chunk.js"
+  },
+  {
+    "revision": "cf1dcab0cb3b59831936",
+    "url": "/static/js/1.cf1dcab0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "baa50885f9e2d2214c29a77574132322",
+    "url": "/index.html"
+  }
+];
