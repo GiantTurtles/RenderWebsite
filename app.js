dcdec3474356c086e28c4b8d23e59e4a2920d07f
Michael Chen <mchen@glenair.com>
2022-08-05 13:20:47 -0700

    added get/post/put functionality

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors')
-const Note = require('./models/note')
+const Note = require('./models/note');
+const { response } = require('express');
+
+
+
 
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+
 
 
 // let notes = [
@@ -15,20 +21,7 @@ app.use(express.static('build'))
 //       content: "HTML is easy",
 //       date: "2022-05-30T17:30:31.098Z",
 //       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only Javascript",
-//       date: "2022-05-30T18:39:34.091Z",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       date: "2022-05-30T19:20:14.298Z",
-//       important: true
-//     }
-//   ]
+//     },]
 
 app.get('/', (request, response) => {
     console.log('PPPPPPO')
@@ -41,34 +34,80 @@ app.get('/api/notes', (request, response) => {
     })
   })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-
-
-    const note = notes.find(note => note.id ===id)
-
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
     if (note) {
-        response.json(note)
+      response.json(note)
     } else {
-        response.status(404).end()
+      response.status(404).end()
     }
-
-
-
-    console.log(note);
-    response.json(note)
+  })
+  .catch(error => next(error))
 })
+
+app.post('/api/notes', (request,response,next) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
+})
+
+
+app.put('/api/notes/:id', (request,response,next) => {
+  const {content,important} = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content,important},
+     {new: true,runValidators:true,context:'query'})
+  .then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
+})
+
 
 app.delete('/api/notes/:id', (request,response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 
-    response.status(204).end()
 })
+
+
+
+
+
 
 
 app.listen(port,() => {
     console.log(`server be running and running on ${port}`)
 })
 
+const errorHandler = (error,request,response,next) => {
+  console.error(error.message)
+
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'wrong id format'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
+    next(error)
+}
+
+app.use(errorHandler)
